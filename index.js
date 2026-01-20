3f1f1090bc5fef7b226a8607fb034af034ed0469
hatanalbokhare-creator <hatanalbokhare@gmail.com>
2026-01-20 21:11:23 +0300

    index.js

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
-// ضروري جداً لحل مشكلة التشفير في ريندر
 const sodium = require('libsodium-wrappers');
+
 (async () => {
-    await sodium.ready;
+  await sodium.ready; // انتظر حتى تكون مكتبة التشفير جاهزة
+
+  const { Client, GatewayIntentBits } = require("discord.js");
+  const { joinVoiceChannel, VoiceConnectionStatus } = require("@discordjs/voice");
+  const http = require("http");
+
+  // خادم وهمي لإبقاء البوت حياً في Render
+  http.createServer((req, res) => {
+    res.write("Bot is online");
+    res.end();
+  }).listen(process.env.PORT || 8080);
+
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildVoiceStates
+    ]
+  });
+
+  const TOKEN = process.env.TOKEN;
+  const GUILD_ID = "1432785665350828185";
+  const VOICE_CHANNEL_ID = "1432785666290356321";
+
+  client.once("ready", () => {
+    console.log(`✅ تم تسجيل الدخول باسم: ${client.user.tag}`);
+
+    const connectToVoice = () => {
+      const guild = client.guilds.cache.get(GUILD_ID);
+      if (!guild) return;
+
+      const connection = joinVoiceChannel({
+        channelId: VOICE_CHANNEL_ID,
+        guildId: GUILD_ID,
+        adapterCreator: guild.voiceAdapterCreator,
+        selfDeaf: false,
+        selfMute: true
+      });
+
+      connection.on(VoiceConnectionStatus.Disconnected, () => {
+        setTimeout(connectToVoice, 5000);
+      });
+    };
+
+    connectToVoice();
+    console.log("🔊 البوت متصل ومستقر الآن.");
+  });
+
+  client.login(TOKEN);
+
 })();
-
-const { Client, GatewayIntentBits } = require("discord.js");
-const { joinVoiceChannel, VoiceConnectionStatus } = require("@discordjs/voice");
-const http = require("http");
-
-// خادم وهمي لإبقاء البوت حياً في ريندر
-http.createServer((req, res) => {
-  res.write("Bot is online");
-  res.end();
-}).listen(process.env.PORT || 8080);
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates
-  ]
-});
-
-const TOKEN = process.env.TOKEN;
-const GUILD_ID = "1432785665350828185";
-const VOICE_CHANNEL_ID = "1432785666290356321";
-
-client.once("ready", () => {
-  console.log(`✅ تم تسجيل الدخول باسم: ${client.user.tag}`);
-
-  const connectToVoice = () => {
-    const guild = client.guilds.cache.get(GUILD_ID);
-    if (!guild) return;
-
-    const connection = joinVoiceChannel({
-      channelId: VOICE_CHANNEL_ID,
-      guildId: GUILD_ID,
-      adapterCreator: guild.voiceAdapterCreator,
-      selfDeaf: false,
-      selfMute: true
-    });
-
-    connection.on(VoiceConnectionStatus.Disconnected, () => {
-      setTimeout(connectToVoice, 5000);
-    });
-  };
-
-  connectToVoice();
-  console.log("🔊 البوت متصل ومستقر الآن.");
-});
-
-client.login(TOKEN);
